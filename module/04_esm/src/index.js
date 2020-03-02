e52fc410a7920d085f831de88fa25bca8f8dfb98
童振宇 <tongzhenyu@hellobike.com>
2020-03-02 22:07:05 +0800

    修改文档

--- module/04_esm/src/index.js
+++ module/04_esm/src/index.js
@@ -1,3 +1,5 @@
 import { name as m1_name } from './module1.js';
+import m from './module1.js';
 import { default as m2_name } from './module2.js';
 console.log(m1_name, m2_name); // module1 module2
+console.log(m);
--- module/04_esm/src/module1.js
+++ module/04_esm/src/module1.js
@@ -1,3 +1,4 @@
 var name = 'module1';
-
+console.log('module1 即将导出变量name');
 export { name };
+export default 1;
